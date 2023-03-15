fd4f3cfbd669cfd1f2f954aab5d9af7fcba66068
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-15 10:53:13 -0700

    validation errors remaining

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,8 @@
 class AuthorsController < ApplicationController
+
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # # added rescue_from
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -8,8 +12,9 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -17,5 +22,14 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  # private
+
+  # def render_not_found_response
+  #   render json: { error: "Author not found" }, status: :not_found
+  # end
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # # added rescue_from
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     post = Post.find(params[:id])
     
@@ -8,10 +12,11 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
     render json: post
+
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +24,18 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+
+  private
+  
+  def author_params
+    params.permit(:email, :name)
+  end
+
+  # def render_not_found_response
+  #   render json: { error: "post not found" }, status: :not_found
+  # end
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
 
 end
